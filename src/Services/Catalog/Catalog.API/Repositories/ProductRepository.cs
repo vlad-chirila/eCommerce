960f3ea9581112e18c7a76b5b89e1542500b1c08
Vlad Chirila <vlad.chirila@live.com>
2022-08-12 17:01:23 +0300

    Add product repository interface and class

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace Catalog.API.Repositories
+{
+    using Catalog.API.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
